cea58d419449a92fec5250ea00ed270a677c78c0
colbygarland <colbygarland@gmail.com>
2015-01-19 13:55:56 -0700

    updated some bugs, remove still not working

--- lab1/database.py
+++ lab1/database.py
@@ -9,9 +9,9 @@ def add_employee():
    
     while True:
         success = True
-        infile = open("test", "r")
+        infile = open("database", "r")
         print("\nEnter a user id, first name, last name and department as so:")
-        print("--userID:fname:lname:department--\n")
+        print("    userID:fname:lname:department    \n")
 
         record = input()
 
@@ -26,16 +26,16 @@ def add_employee():
                 
         if success:
             infile.close()
-            infile = open("test", "a")
+            infile = open("database", "a")
             infile.write(str(userID) + ":" + fname + ":" + lname + ":" + dept + "\n")
             infile.close()
-            print("\nAddition Successful")
+            print("\n----Addition Successful.")
         else:
             infile.close()
-            print("\nEmployee already in database")
+            print("\nEmployee is already in database.")
 
         print("Do you wish to add another employee?")
-        print("Enter y for yes, n to return to main menu")
+        print("Enter 'y' for yes, 'n' to return to Main Menu.")
         addAnother = input()
         if addAnother[0] == 'n' or addAnother[0] == 'N':
             break
@@ -43,34 +43,37 @@ def add_employee():
 #Searches for an employee
 def search_employee():
     print("\n----------Employee FMS - Searching for Employee----------")
+    
 
     while True:
+        check = False
         print("Enter an ID to search for:")
         try:
             ID = input()
             ID = int(ID)
         except ValueError as err:
-            print("Employee ID must be a number.")
+            print("EMPLOYEE ID MUST BE A NUMBER.")
             continue
  
-        infile = open("test", "r")
+        infile = open("database", "r")
 
         for rec in infile:
             userID, rest = rec.split(":", 1)
             userID = int(userID)
             if ID == userID:
                 fname, lname, dept = rest.split(":")
-                print("\nUserID:" + str(ID) + " " + fname + " " + lname + " " + dept + "\n")
-                break
-            else:
-                print("\nUserID:" + str(ID) + " does not exist.")
+                print("\nUserID:" + str(ID) + " " + fname + " " + lname + " " + dept)
+                check = True
                 break
 
-        print("\nWould you like to search for another employee?")
-        print("Enter y for yes, n to return to main menu")
+        if check == False:
+           print("\nUserID:" + str(ID) + " does not exist.\n")
+
+        print("Would you like to search for another employee?")
+        print("Enter 'y' for yes, 'n' to return to Main Menu.")
         addAnother = input()
         if addAnother[0] == 'n' or addAnother[0] == 'N':
-            inline.close()
+            infile.close()
             break
 
 #Removes an employee specified by the user id
@@ -88,7 +91,7 @@ def remove_employee():
             print("Employee ID must be a number.")
             continue
 
-        f = open("test", "r")
+        f = open("database", "r")
         for rec in f:
             searchID, rest = rec.split(":",1)
             searchID = int(searchID)
@@ -98,17 +101,17 @@ def remove_employee():
         f.close()
 
         if deleteOkay:
-            print("Employee found - are you sure you wish to delete?")
-            print("Enter y for yes, n to return to main menu")
+            print("Employee found - Are you sure you wish to delete?")
+            print("Enter 'y' for yes, 'n' to return to Main Menu.")
             yes = input()
             if yes[0] == 'n' or yes[0] == 'N':
                 break
 
         if deleteOkay:
-            f = open("test", "r")
+            f = open("database", "r")
             lines = f.readlines()
             f.close()
-            f = open("test", "w")
+            f = open("database", "w")
             ID = str(ID)
             for line in lines:
                 userID, rest = lines.split(":",1) #fix this part
@@ -123,28 +126,19 @@ def remove_employee():
 
 #Prints the database 
 def display_all():
-    inline = open("test", "r")
+    inline = open("database", "r")
+    emptiness = True
     print("\nEmployees in the database:\n")
     for rec in inline:
         ID, fname, lname, dept = rec.split(":")
         print(ID, fname, lname, dept, end="")
+        emptiness = False
+
+    if emptiness:
+        print("Employee database is currently empty.\n")
+
     inline.close()
     print("\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     add_employee()
