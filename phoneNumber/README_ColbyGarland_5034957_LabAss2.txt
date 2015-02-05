211b64d4ea790d916ce1cfcb928cedd3851af704
colbygarland <colbygarland@gmail.com>
2015-02-05 10:37:40 -0700

    done, so far no errors

---
+++ phoneNumber/README_ColbyGarland_5034957_LabAss2.txt
@@ -0,0 +1,24 @@
+README for the phone number lab for CS3130.
+--
+--
+Author: Colby Garland
+ID#   : 5034957
+--
+--
+Purpose: Using regular expressions, parse a phone number
+--
+--
+Git URL: https://github.com/colbygarland/cs3130.git
+--
+--
+Project is under the "phoneNumber" directory in the git repository above.
+--
+--
+--
+Weaknesses:
+
+--
+--
+
+--
+--
---
+++ phoneNumber/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+""" Phone number lab assignment #2 for 
+    cs3130
+    Author: Colby Garland
+    ID#   : 5034957 """
+
+import re
+
+def main():
+    menu()
+    
+
+""" Prints the menu and accepts input """
+def menu():
+    print("--")
+
+    while True:
+        print("Enter a 10 digit phone number: ", end="")
+        num = input()
+        if num == '':
+            break
+        else:
+            rx1 = r'(\d{3})[- )]*(\d{3})[- )]*(\d{4})'
+
+            if len(num) < 10:
+                print("Sorry phone number needs exactly 10 digits")
+            else:
+                phoneNum = re.search(rx1, num)
+                try:
+                    areaCode, first3, last4 = phoneNum.groups()
+                    print("Number is " + "(" + areaCode + ")" + " " + first3 + " " + last4)
+                except AttributeError as err:
+                    print("Characters other than digits, hypens, space and parantheses detected")
+                
+
+main()
+    
